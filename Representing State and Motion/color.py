ef65a951d371ec9d32c1ccf77ac7e7c5e21372dc
chavasaiteja <chavasaiteja12345@gmail.com>
2019-08-01 09:59:47 -0700

    Add operator overloading python

--- Representing State and Motion/color.py
+++ Representing State and Motion/color.py
@@ -7,29 +7,34 @@ attributes:
     g - a value between 0-255 for green
     b - a value between 0-255 for blue
 '''
-    
+
 class Color(object):
-    
-    # __init__ is called when a color is constructed using color.Color(_, _, _)
+
+    # Initializes a color with rgb values
     def __init__(self, r, g, b):
-        # Setting the r,g,b values
         self.r = r
         self.g = g
         self.b = b
-        
 
-    # __repr__ is called when a color is printed using print(some_color)
-    # It must return a string
+    # Called when a Color object is printed out
     def __repr__(self):
-        '''Display a color swatch and then return a text description of r,g,b values.'''
+        '''Display a color swatch and returns a text description of r,g,b values'''
         
         plt.imshow([[(self.r/255, self.g/255, self.b/255)]])
         
-        ## TODO: Write a string representation for the color
-        ## ex. "rgb = [self.r, self.g, self.b]"
-        ## Right now this returns an empty string
-        string = 'rgb = [{}, {}, {}]'.format(self.r, self.g, self.b)
-        
-        return string
+        return 'r, g, b = ' + str(self.r) + ', ' + str(self.g) + ', ' + str(self.b)
     
-    
+
+    ## TODO: Complete this add function to add two colors together
+    def __add__(self, other):
+        '''Adds the r, g, and b components of each color together 
+           and averaging them. 
+           The new Color object, with these averaged rgb values, 
+           is returned.'''
+        r = (self.r + other.r) / 2
+        g = (self.g + other.g) / 2
+        b = (self.b + other.b) / 2
+
+        return Color(r, g, b)
+
+
